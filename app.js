89a426ef55b1d0df1c28cd40fbe409a03f1f0794
M. C. DeMarco <M. C. DeMarco@localhost>
2018-03-06 18:56:38 -0500

    Remove vuex.

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
-//js/store/index.js
-
 var app = new Vue({
   el: '#app',
-	store,
 	data: {
-				show: {play: false, score: false, rules: true},
-				rules: {setup: false, buy: false, refill: false, play: false, end: false, score: false},
-				suits: [{name: 'moons', length: 0, ace: false, crown: false, unspent: false},
-								{name: 'suns', length: 0, ace: false, crown: false, unspent: false},
-								{name: 'waves', length: 0, ace: false, crown: false, unspent: false},
-								{name: 'leaves', length: 0, ace: false, crown: false, unspent: false},
-								{name: 'wyrms', length: 0, ace: false, crown: false, unspent: false},
-								{name: 'knots', length: 0, ace: false, crown: false, unspent: false}],
-				discards: 0,
-				unfilled: 0,
-				scoreChart: [{count: 0, vp: -5},
-										 {count: 1, vp: -5},
-										 {count: 2, vp: 2},
-										 {count: 3, vp: 5},
-										 {count: 4, vp: 9},
-										 {count: 5, vp: 14},
-										 {count: 6, vp: 20},
-										 {count: 7, vp: 30},
-										 {count: 8, vp: 30},
-										 {count: 9, vp: 30},
-										 {count: 10, vp: 30},
-										 {count: 11, vp: 30}]
+		extended: false,
+		show: {play: false, score: false, rules: true},
+		rules: {setup: false, buy: false, refill: false, play: false, end: false, score: false},
+		suits: [{name: 'moons', length: 0, ace: false, crown: false, unspent: false},
+						{name: 'suns', length: 0, ace: false, crown: false, unspent: false},
+						{name: 'waves', length: 0, ace: false, crown: false, unspent: false},
+						{name: 'leaves', length: 0, ace: false, crown: false, unspent: false},
+						{name: 'wyrms', length: 0, ace: false, crown: false, unspent: false},
+						{name: 'knots', length: 0, ace: false, crown: false, unspent: false}],
+		discards: 0,
+		unfilled: 0,
+		scoreChart: [{count: 0, vp: -5},
+								 {count: 1, vp: -5},
+								 {count: 2, vp: 2},
+								 {count: 3, vp: 5},
+								 {count: 4, vp: 9},
+								 {count: 5, vp: 14},
+								 {count: 6, vp: 20},
+								 {count: 7, vp: 30},
+								 {count: 8, vp: 30},
+								 {count: 9, vp: 30},
+								 {count: 10, vp: 30},
+								 {count: 11, vp: 30}]
   },
 	computed: {
-		extended: function () {
-			return store.state.modules.settings.getters.extended;
-		},
 				acbTotal: function () {
 					var total = 0;
 					Object.entries(this.suits).forEach(([key, val]) => {
@@ -70,7 +65,7 @@ var app = new Vue({
 				}
 			},
 			methods: {
-				extend: function(e) {this.$store.dispatch('toggleExtendedDeck');}, 
+				extend: function(e) {this.extended = !this.extended;}, 
 				getSuitName: function(idx) {return this.suits[idx-1].name;},
 				rando: function(intr) {return Math.floor(Math.random() * intr + 1);}
 			}
